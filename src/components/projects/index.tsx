9b398e5fc382ea28e7ac571293b1a8fee0111edd
andresceballosm <andres.ceballos@devbase.us>
2023-05-01 18:23:00 -0500

    update tables

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -3,7 +3,7 @@ import { PROJECTS } from '@/components/projects/constants/projects';
 
 const ProjectsSection = () => (
   <section className={tw('bg-white pb-6')}>
-    <div className={tw('max-w-7xl mx-auto p-4 sm:p-6 lg:p-8')}>
+    <div className={tw('max-w-7xl mx-auto p-4 sm:p-4 lg:p-8')}>
       <div className={tw('container mx-auto px-6 p-6 bg-white')}>
         <div className={tw('mb-16 text-center')}>
           <h4 className={tw('text-base text-indigo-600 font-semibold tracking-wide uppercase')}>Projects</h4>
@@ -41,13 +41,13 @@ const ProjectsSection = () => (
                         <div className="font-normal text-gray-500">{item.description}</div>
                       </div>
                     </th>
-                    <td className="px-6 py-4">{item.platform}</td>
-                    <td className="px-6 py-4">{item.sector}</td>
-                    <td className="px-6 py-4">
+                    <th className="px-4 py-4">{item.platform}</th>
+                    <th className="px-4 py-4">{item.sector}</th>
+                    <th className="px-6 py-4">
                       <a href={item.url} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
                         Visit
                       </a>
-                    </td>
+                    </th>
                   </tr>
                 ))}
               </tbody>
